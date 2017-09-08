28027eb357b09e71a311b2700c1d6220d7542028
Patrick Schreiber <patrick.schreiber2@student.kit.edu>
2017-09-08 15:30:46 +0200

    bump version to 0.2.0

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 from .core import *
 
+import os
+__version__ = open(os.path.join(os.path.dirname(__file__),"VERSION"), "r").read().strip()
+
 def is_sad():
     print("Lisa is sad.")
     return True
