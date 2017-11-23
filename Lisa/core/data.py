0a445393e1f23e646a36123b4ffc1569ac389acb
Patrick Schreiber <patrick.schreiber2@student.kit.edu>
2017-11-23 11:29:35 +0100

    update unit conversion and add cps/aps, cpev/apev, cpspev/apspev

--- Lisa/core/data.py
+++ Lisa/core/data.py
@@ -10,9 +10,9 @@ if config_options.get("use_cython"):
     except ImportError as e:
         print(e)
         print("Fallback to python version")
-        from .file import File
+        from .file import File, Axis
 else:
-    from .file import File
+    from .file import File, Axis
 from .internals import lisa_print
 from .utils import attr_from_unit, UnitError, DataNotInFile
 import numpy as np
@@ -63,6 +63,7 @@ class Data(object):
                 else:
                     return getattr(self._file, attr)()[what]
             return inner
+
         def inner(idx, *args, **kwargs):
             unit = ''
             if len(args) > 0 and isinstance(args[0], str):
@@ -74,32 +75,73 @@ class Data(object):
             if unit == '':
                 raise UnitError("No unit given.")
             data = getattr(self._file, attr)(idx)
-            conversion_attribute = attr_from_unit(unit, self.version)
 
-            if kwargs.get("sub_index", None) is not None:
-                if 'sub_idx' in kwargs:
-                    raise AssertionError("sub_idx or sub_index are to be used, not both")
-                kwargs['sub_idx'] = kwargs.pop("sub_index")
+            sub_index = kwargs.get("sub_index", kwargs.get("sub_idx", None))
 
-            if conversion_attribute is None:
-                if kwargs.get("sub_idx", None) is None:
+            factor = self.conversion_factor(unit, attr, data)
+
+            if factor is None:
+                if sub_index is None:
                     return data * np.float64(1.0)
                 else:
-                    return data[kwargs.get("sub_idx")] * np.float64(1.0)
+                    return data[sub_index] * np.float64(1.0)
+            if sub_index is None:
+                return data*factor
             else:
-                if attr == "impedance" and conversion_attribute not in data.attrs:
-                    attrs = self._file.impedance("datagroup").attrs
-                else:
-                    attrs = data.attrs
+                return data[sub_index] * factor
 
-            if conversion_attribute in attrs:
-                if kwargs.get("sub_idx", None) is None:
-                    return data*attrs[conversion_attribute]
-                else:
-                    return data[kwargs.get("sub_idx")] * attrs[conversion_attribute]
-            else:
-                lisa_print("units for this data", list(attrs))
-                raise UnitError(unit+" is not a valid unit for this data.")
         inner.__doc__ = self.__getattr__.__doc__
         inner.__name__ = attr
         return inner
+
+    def conversion_factor(self, unit, attr, data):
+        """
+        Get the conversion factor for unit for attr from data
+        :param unit: The physical unit to use (lowercase!)
+        :param attr: The attribute (the data e.g. bunch_profile)
+        :param data: The actual data (h5 object) to extract the conversion factor from
+        :return: float, conversion_factor
+        """
+        if unit is None:
+            return None
+        if unit == "cps" or unit == "aps":
+            try:
+                coa = attr_from_unit(unit[0]+"pnbl", self.version)
+                coa = self._file.bunch_profile(Axis.DATA).attrs[coa]
+                s = attr_from_unit("s", self.version)
+                s = self._file.bunch_profile(Axis.XAXIS).attrs[s]
+                return coa/s
+            except KeyError as e:
+                raise UnitError("Conversion failure, cannot find attribute for "+str(e).split("'")[-2])
+        if unit == "cpev" or unit == "apev":
+            try:
+                coa = attr_from_unit(unit[0]+"pnes", self.version)
+                coa = self._file.energy_profile(Axis.DATA).attrs[coa]
+                ev = attr_from_unit("eV", self.version)
+                ev = self._file.energy_profile(Axis.EAXIS).attrs[ev]
+                return coa/ev
+            except KeyError as e:
+                raise UnitError("Conversion failure, cannot find attribute for "+str(e).split("'")[-2])
+        if unit == "cpspev" or unit == "apspev":
+            try:
+                coa = attr_from_unit(unit[0]+"pnblpnes", self.version)
+                coa = self._file.bunch_profile(Axis.DATA).attrs[coa]
+                ev = attr_from_unit("eV", self.version)
+                ev = self._file.energy_profile(Axis.EAXIS).attrs[ev]
+                s = attr_from_unit("s", self.version)
+                s = self._file.bunch_profile(Axis.XAXIS).attrs[s]
+                return coa/ev/s
+            except KeyError as e:
+                raise UnitError("Conversion failure, cannot find attribute for "+str(e).split("'")[-2])
+
+        conversion_attribute = attr_from_unit(unit, self.version)
+        if attr == "impedance" and conversion_attribute not in data.attrs:
+            attrs = self._file.impedance("datagroup").attrs
+        else:
+            attrs = data.attrs
+        if conversion_attribute in attrs:
+            return attrs[conversion_attribute]
+        else:
+            lisa_print("units for this data", list(attrs))
+            raise UnitError(unit+" is not a valid unit for this data.")
+
