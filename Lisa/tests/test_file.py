c4943f5dc971d8a68ab4d71c32d8907de6f3d209
Patrick Schreiber <patrick.schreiber2@student.kit.edu>
2018-02-27 14:44:36 +0100

    Fix test to reflect bunch_length calculations

--- Lisa/tests/test_file.py
+++ Lisa/tests/test_file.py
@@ -1,7 +1,7 @@
 # from unittest_helpers import CustomTestCase
 import h5py
 import numpy as np
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_array_equal, assert_allclose
 
 # rethink importing it
 import Lisa
@@ -44,6 +44,7 @@ class FileTest(unittest.TestCase):
             s.YDATA: "data/y",  # SourceMap
             "datagroup": "data"
         }
+        self._blfunc = None
 
     def do_test(self, file):
         f = file
@@ -61,27 +62,38 @@ class FileTest(unittest.TestCase):
                         else:
                             self.assertListEqual(np.array(getattr(obj, spec)).tolist(), np.array(obj[idx]).tolist())
                 for spec in self.specs[par]:  # test for axis and data
+                    if spec == "datagroup":  # skip because only in impedance and REAL and IMAG get tested individually anyway
+                        continue 
+
                     with self.subTest(msg=par, spec_type=spec):
                         obj = getattr(f, par)(spec)
                         if spec in self._axis_datasets:  # Check for Axis data
                             if isinstance(obj, h5py.Dataset):  # if it is a h5py dataset we can compare them directly
                                 self.assertEqual(obj, f.file.get(self._axis_datasets[spec]))
                             else:
-                                self.assertListEqual(np.array(obj).tolist(),
-                                                     np.array(f.file.get(self._axis_datasets[spec])).tolist())
+                                assert_allclose(np.array(obj),np.array(f.file.get(self._axis_datasets[spec])))
                         elif spec in self._data_datasets:  # Check for Data data
                             if isinstance(obj, h5py.Dataset):  # if it is a h5py dataset we can compare them directly
                                 self.assertEqual(obj, f.file.get(f._met2gr[par]).get(self._data_datasets[spec]))
                             else:
-                                self.assertListEqual(np.array(obj).tolist(),
-                                                     np.array(f.file.get(f._met2gr[par]).
-                                                              get(self._data_datasets[spec])).tolist())
+                                if par == "bunch_length":
+                                    if self._blfunc is None:
+                                        data = np.array(f.file.get(f._met2gr[par]).get(self._data_datasets[spec]))
+                                    elif self._blfunc == "sqrt":
+                                        data = np.sqrt(np.array(f.file.get(f._met2gr[par]).get(self._data_datasets[spec])))
+                                    elif self._blfunc == "calc_bl":
+                                        data = Lisa.data.utils.calc_bl(np.array(f.file.get(self._axis_datasets[s.XAXIS])), 
+                                                np.array(f.file.get(f._met2gr["bunch_profile"]).get(self._data_datasets[spec])))
+                                else:
+                                    data = np.array(f.file.get(f._met2gr[par]).get(self._data_datasets[spec]))
+                                assert_allclose(np.array(obj), data)
                         else:
                             raise Exception("Error")
 
     def test_data_version_15(self):
         f = Lisa.File(op.join(self.file_dir_path, "v15-1.h5"))
         self.specs["source_map"] = [s.XAXIS, s.EAXIS, s.XDATA, s.YDATA]
+        self._blfunc = "calc_bl"
         self.do_test(f)
 
     def test_parameters_version_15(self):
@@ -91,6 +103,7 @@ class FileTest(unittest.TestCase):
     def test_data_version_14(self):
         f = Lisa.File(op.join(self.file_dir_path, "v14-1.h5"))
         self.specs["source_map"] = [s.XAXIS, s.EAXIS, s.XDATA, s.YDATA]
+        self._blfunc = "calc_bl"
         self.do_test(f)
 
     def test_parameters_version_14(self):
@@ -99,6 +112,7 @@ class FileTest(unittest.TestCase):
 
     def test_data_version_13(self):
         f = Lisa.File(op.join(self.file_dir_path, "v13-2.h5"))
+        self._blfunc = "sqrt"
         self.do_test(f)
         with self.subTest(msg="Raises", what="source_map"):
             with self.assertRaises(DataNotInFile):
@@ -122,25 +136,28 @@ class FileTest(unittest.TestCase):
                     with self.subTest(msg=par, spec=spec):
                         self.assertListEqual(np.array(getattr(obj, spec)).tolist(), np.array(obj[idx]).tolist())
                 for spec in self.specs[par]:  # test for axis and data
+                    if spec == "datagroup":  # skip because only in impedance and REAL and IMAG get tested individually anyway
+                        continue 
                     with self.subTest(msg=par, spec_type=spec):
                         obj = getattr(f, par)(spec)
                         if spec in self._axis_datasets:  # Check for Axis data
                             if spec == s.TIME:
-                                assert_array_equal(np.array(obj).tolist(),
-                                                   np.array(f.file.get(self._axis_datasets[spec]))[1:].tolist())
+                                assert_allclose(np.array(obj),
+                                                   np.array(f.file.get(self._axis_datasets[spec]))[1:])
                             else:
-                                assert_array_equal(np.array(obj).tolist(),
-                                                   np.array(f.file.get(self._axis_datasets[spec]))[0].tolist())
+                                assert_allclose(np.array(obj),
+                                                   np.array(f.file.get(self._axis_datasets[spec]))[0])
 
                         elif spec in self._data_datasets:  # Check for Data data
                             if s.TIME in self.specs[par]:
-                                assert_array_equal(np.array(obj).tolist(),
-                                                   np.array(f.file.get(f._met2gr[par]).
-                                                            get(self._data_datasets[spec]))[1:].tolist())
+                                if par == "bunch_length":
+                                    blfunc = np.sqrt
+                                else:
+                                    blfunc = lambda x: x
+                                assert_allclose(np.array(obj), 
+                                        blfunc(np.array(f.file.get(f._met2gr[par]).get(self._data_datasets[spec]))[1:]))
                             else:
-                                assert_array_equal(np.array(obj).tolist(),
-                                                   np.array(f.file.get(f._met2gr[par]).
-                                                            get(self._data_datasets[spec])).tolist())
+                                assert_allclose(np.array(obj), np.array(f.file.get(f._met2gr[par]).get(self._data_datasets[spec])))
                         else:
                             raise Exception("Error")
 
